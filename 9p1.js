d103264e8d23f2d73d92965f9c378423d7d85b8d
Dipam Patel <dp482@njit.edu>
2021-06-21 01:12:57 -0400

    Object lec and split practice problems

--- 9p1.js
+++ 9p1.js
@@ -1,3 +1,5 @@
+//How to store player or persons inforamtion without knowing about Objects
+
 var playing11 = ["kohli", "RS", "Gill", "Pujara","Rahane","Pant", "Jadeja", "Ashwin", "Shami", "Bumrah", "Ishant"]
 var age = [31,30,23,34,32,23,29,34,33,28,30]
 var height = [5, 6, 5, 6, 5, 7, 5 , 6,5 ,6 ,5]
@@ -5,13 +7,17 @@ var runs = [7000, 100, 200, 300, 400, 500, 600 , 700 , 800, 900, 950]
 var wickets = [1,2,3,4,5,6,7,8,9,10,11]
 console.log(playing11, age, height, runs, wickets)
 
+/*Learned - How to use array to store inforamtion of players details*/
 
-let player_1={name:"Rohit",
-			age:31,
+
+//Intro to OBJECT
+
+let player_1={name:"Rohit",						//use let to start declaring the object(Player_1 is handler)
+			age:31,							    // everthing inside the handle is known as object with different properties and value
 			height:5,
 			runs:3000,
 			wickets:10}
-console.log(player_1.runs)
+console.log(player_1.runs)						//Use handler/object name with (.) and property name to print it's value
 
 let player_2={name:"Kohli",
 			age:30,
@@ -19,3 +25,6 @@ let player_2={name:"Kohli",
 			runs:5000,
 			wickets:8}
 console.log(player_2.name)
+
+/* Learned - Object makes it easier for user to store details in a proper way where it makes easy
+for to access/update the inforamtions.*/
--- 9p2.js
+++ 9p2.js
@@ -1,35 +1,47 @@
-//Creating Object
+//Creating Object cont
 
-let student = {"name":"Dipam",
-			phoneNum: "(551)226-3521",
-			email_Id: "dp482@njit.edu",
+let student = {"name":"Dipam",							
+			phoneNum: "(551)226-3521",				   //Using normal property name
+			email_Id: "dp482@njit.edu",			
 			college: "NJIT",
 			Degree: "B.S. CS",
-			"Birth Date": "10/05/1998",
+			"Birth Date": "10/05/1998",					//To call a key value with space no special characters you must put the property name in quotation("")
 			Specs: false,
-			car : ["Camry", "Audi", "Range"]
-			}  //to call this property student["Birth Date"]
-student.age=22
-delete student.age
-console.log(student)
+			car : ["Camry", "Audi", "Range"]			//storing the car list as array
+			} 
+student.age=22											// adding a new property
+delete student.age									    //to delete the age property & it value
+console.log(student)									//to call this property student["Birth Date"]
 
 
 Object
 
 let student = {"name":"Dipam",
 			phoneNum: "(551)226-3521",
-			email_Id: "dp482@njit.edu",
+			email_Id: "dp482@njit.edu",					//Property name with (_) doesn't need quotation
 			college: "NJIT",
-			Degree: "B.S. CS",
+			Degree: "B.S. CS",							
 			"Birth Date": "10/05/1998",
-			Specs: false,
+			Specs: false,								//assign boolean val for one of the property
 			car : ["Camry", "Audi", "Range"]
-			}  //to call this property student["Birth Date"]
-student.age=22
-//  student.car.push("Honda", "KIA")
-// console.log("car" in student)
-console.log(Object.keys(student))
+			}  											//to call this property student["Birth Date"]
+student.age=22											// adding age key val using handler/object (student.age)
+//  student.car.push("Honda", "KIA")					//Using push to add value in the end of array list
+// console.log("car" in student)						//Check if the keyvalue is in the object
+console.log(Object.keys(student))						//Print all the property inside the handler student 
+														//.keys is just a method of object
+
 // console.log(student)
 
 
-//
+/*Learned - Obejct = store multiple values with property into a one memory address. The values can be ("String", num, boolean, etc)
+Handler/ Object/ Properties
+ creating object
+ calling peoperty(key value) = (.) or ([""])
+ property name with space need to be stored as a string
+ adding = Handler.Property = Val
+ removing = delete handlerName.propertyName
+ Storing at the end of array = Handler.property.push("Val1","Val2")
+ to check if certain properties exist in an object = concole.log("propery" in object) = return true or false
+ Print list of property = Object.keys(handler)
+ print val = Object.value(handler)*/
