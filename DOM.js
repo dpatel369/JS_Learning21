1b2f8a715c9d2fce104fdb45b6af05cfdf015056
Dipam Patel <dp482@njit.edu>
2021-08-10 02:07:19 -0400

    Intro to DOM and chess webpage

---
+++ DOM.js
@@ -0,0 +1,25 @@
+//Browser Environment - web
+// JS became famous cuz it's the only language that runs on every browser.
+
+//Browser - display a page/ html page
+// 3 elements of the page 
+//HTML - structure 
+// CSS - Style 
+// JS - Behavior
+//ALL DATA is string and every tag become a object(that object is nodes)
+// Browser sends HTTP request (TCP connection) send respone
+//DOM - tree of nodes (Document object model)
+//HTML data comes to browser with HTTP request and than one component browser converts those data into DOM (nodes tree) (Creation of the DOM) [Model logic]
+//Other componet look at and  draws the DOM on the screen that is called (RENDERING DOM) [View Logic]
+//User gesture(clicking, filling the form) [Controller logic]
+// 3 logics = MVC 
+
+//JS can touch DOM and create node and add node to DOM
+//You can select any tag using the ID (document.getElementByID(x))
+//Blocklevel text one div after other div = new line (Text come on new line)
+//In-line text
+//<div><span> - grouping text 
+//<span> - in one line
+
+//Position absolute - write top and left (works with div as parent)
+//Position fixed - according to the browser
